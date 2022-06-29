0c2db7ff1b25bc45d86764cae52e60ef391fed28
Davit Gujejiani <66127957+DavitGujejiani@users.noreply.github.com>
2022-06-29 22:23:14 +0400

    moved slider to homepage component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from "./components/header/Header";
-import Slider from "./components/slider/Slider";
 import Footer from "./components/footer/footer";
+import Homepage from "./components/homepage/Homepage";
 
 const App = () => {
     return (
@@ -9,7 +9,7 @@ const App = () => {
             <Footer/>
             <div className="px-5">
                 <Header/>
-                <Slider/>
+                <Homepage/>
             </div>
         </>
 
---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Slider from "./slider/Slider";
+
+const Header = () => {
+    return (
+        <>
+            <Slider/>
+        </>
+    );
+};
+
+export default Header;
