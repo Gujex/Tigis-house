03837b19b65f23d8d23bbb6be580df642a95f6c3
macbookpro <giorgi.gujejiani.1@iliauni.edu.ge>
2022-07-08 12:57:41 +0400

    some style has improved

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Slider from "./slider/Slider";
-import DemandedProductsSlider from "./DemandedProductsSlider/DemandedProductsSlider";
 
 const Header = () => {
     return (
         <>
             <Slider/>
-            <DemandedProductsSlider />
         </>
     );
 };
---
+++ src/components/productCarousel/carouselTitle/CarouselTitle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./carouselTitle.css"
+
+const CarouselTitle = () => {
+    return (
+        <div className="pt-5 text-center mt-25">
+            <p style={{letterSpacing: '0.3em'}} className="sub_title mt-5 text-uppercase fs-12 opacity-75 fw-600 font-monospace ">Tigis Shop</p>
+            <h1 className=" mt-3 mb-3" style={{fontFamily: 'Marcellus'}}>Customer beauty essentials</h1>
+            <p className="sub_title opacity-50">Removes all traces of dirt, makeup & sunscreen.</p>
+        </div>
+    );
+};
+
+export default CarouselTitle;
